0ca9949110f6da5dc935cc8e1893b40d43cde689
Md. Ashfaq Sadat <68952956+ashfaq-sadat@users.noreply.github.com>
2023-09-02 16:32:35 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function changeText() {
+	document.getElementById("h2").innerHTML = "This is a changed text";
+}
+function resetText() {
+	document.getElementById("h2").innerHTML =
+		"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, quasi, dolor quidem adipisci excepturifugiat nobis quod, illum id doloribus sapiente saepe similique ipsam reprehenderit suscipit dolore doloremque rem voluptatibus!";
+}
+function changeColor() {
+	document.getElementById("h21").style.color = "blue";
+}
+
+function resetColor() {
+	document.getElementById("h21").style.color = "black";
+}
+
+function changeWidth() {
+	document.getElementById("h211").style.fontWeight = 800;
+}
+function thinWidth() {
+	document.getElementById("h211").style.fontWeight = 400;
+}
+function resetWidth() {
+	document.getElementById("h211").style.fontWeight = 700;
+}
